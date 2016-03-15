0dfdbae4cfc50e3229616c8c6d882f16fe25ca2a
intersim <emily.intersimone@gmail.com>
2016-03-14 22:55:19 -0400

    updated server port AGAIN

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var http = require('http');
 var server = http.createServer();
 
+var port = process.env.PORT || 3001;
+
 server.on('request', require('./app'));
 
-server.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, server.settings.env);
+server.listen(port, function(){
+  console.log("Express server listening on port " + port);
 });
